a2dd565b9a4c685c81f89ceb59c3740b96618b71
jpedropmont <jpedropmont@gmail.com>
2023-02-19 17:40:53 -0300

    Adicionando novo endpoint para ativmob

--- src/main/java/br/com/a2dm/spdmws/ws/AtivMobWS.java
+++ src/main/java/br/com/a2dm/spdmws/ws/AtivMobWS.java
@@ -1,26 +1,39 @@
 package br.com.a2dm.spdmws.ws;
 
-import br.com.a2dm.brcmn.dto.ClienteIntegracaoDTO;
+import br.com.a2dm.brcmn.dto.ativmob.EventDTO;
+import br.com.a2dm.spdm.ativmob.service.AtivMobService;
 import br.com.a2dm.spdm.exception.ApiException;
 import br.com.a2dm.spdm.exception.ExceptionUtils;
-import br.com.a2dm.spdm.omie.service.OmieClienteService;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
 
 @Path("/ativmob")
 public class AtivMobWS {
 
-    @GET
-    @Path("/eventos/{cnpj}")
+//    @GET
+//    @Path("/eventos/{cnpj}")
+//    @Produces(MediaType.APPLICATION_JSON)
+//    public List<EventDTO> buscarEventos(@PathParam("cnpj") BigInteger cnpj) throws ApiException {
+//        try {
+//            return AtivMobService.getInstance().buscarEvents(cnpj);
+//        } catch (Exception e) {
+//            throw ExceptionUtils.handlerApiException(e);
+//        }
+//    }
+
+    @POST
+    @Path("/events/{cnpj}")
     @Produces(MediaType.APPLICATION_JSON)
-    public ClienteIntegracaoDTO buscarEventos(@PathParam("cnpj") BigInteger cnpj) throws ApiException {
+    public void salvarEvents(@PathParam("cnpj") BigInteger cnpj) throws ApiException {
         try {
-            return OmieClienteService.getInstance().pesquisarCliente(cnpj);
+            AtivMobService service = AtivMobService.getInstance();
+            ArrayList<EventDTO> events = service.buscarEvents(cnpj);
+            service.marcarLidoEvents(events);
+            service.salvarEvents(events);
         } catch (Exception e) {
             throw ExceptionUtils.handlerApiException(e);
         }
