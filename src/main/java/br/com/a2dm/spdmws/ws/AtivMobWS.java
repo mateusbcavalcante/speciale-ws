3dbaea8772bae265f9bc978eaa14df6cb59d64bb
Mateus Bastos <mateus@treinocertold.com.br>
2023-02-19 19:22:11 -0300

    Refatoracao da ativmob

--- src/main/java/br/com/a2dm/spdmws/ws/AtivMobWS.java
+++ src/main/java/br/com/a2dm/spdmws/ws/AtivMobWS.java
@@ -1,42 +1,30 @@
 package br.com.a2dm.spdmws.ws;
 
-import br.com.a2dm.brcmn.dto.ativmob.EventDTO;
+import java.math.BigInteger;
+import java.util.List;
+
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import br.com.a2dm.brcmn.entity.ativmob.Event;
 import br.com.a2dm.spdm.ativmob.service.AtivMobService;
 import br.com.a2dm.spdm.exception.ApiException;
 import br.com.a2dm.spdm.exception.ExceptionUtils;
 
-import javax.ws.rs.*;
-import javax.ws.rs.core.MediaType;
-import java.math.BigInteger;
-import java.util.ArrayList;
-import java.util.List;
-
 @Path("/ativmob")
 public class AtivMobWS {
-
-//    @GET
-//    @Path("/eventos/{cnpj}")
-//    @Produces(MediaType.APPLICATION_JSON)
-//    public List<EventDTO> buscarEventos(@PathParam("cnpj") BigInteger cnpj) throws ApiException {
-//        try {
-//            return AtivMobService.getInstance().buscarEvents(cnpj);
-//        } catch (Exception e) {
-//            throw ExceptionUtils.handlerApiException(e);
-//        }
-//    }
 
     @POST
     @Path("/events/{cnpj}")
     @Produces(MediaType.APPLICATION_JSON)
-    public void salvarEvents(@PathParam("cnpj") BigInteger cnpj) throws ApiException {
+    public List<Event> proccessEvents(@PathParam("cnpj") BigInteger cnpj) throws ApiException {
         try {
-            AtivMobService service = AtivMobService.getInstance();
-            ArrayList<EventDTO> events = service.buscarEvents(cnpj);
-            service.marcarLidoEvents(events);
-            service.salvarEvents(events);
+            return AtivMobService.getInstance().proccessEvents(cnpj);
         } catch (Exception e) {
             throw ExceptionUtils.handlerApiException(e);
         }
     }
-
 }
