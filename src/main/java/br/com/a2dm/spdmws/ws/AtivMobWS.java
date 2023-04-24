0d763f7549a1f16abfe4a53ec31537e0f529f371
Mateus Bastos <mateus@treinocertold.com.br>
2023-04-24 09:48:00 -0300

    Criacao do metodo para inserir sugestao de pedido unico

--- src/main/java/br/com/a2dm/spdmws/ws/AtivMobWS.java
+++ src/main/java/br/com/a2dm/spdmws/ws/AtivMobWS.java
@@ -3,12 +3,14 @@ package br.com.a2dm.spdmws.ws;
 import java.math.BigInteger;
 import java.util.List;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import br.com.a2dm.brcmn.dto.ativmob.EventDTO;
 import br.com.a2dm.spdm.ativmob.service.AtivMobService;
 import br.com.a2dm.spdm.entity.SugestaoPedido;
 import br.com.a2dm.spdm.exception.ApiException;
@@ -23,6 +25,18 @@ public class AtivMobWS {
     public List<SugestaoPedido> proccessSugestoesPedido(@PathParam("cnpj") BigInteger cnpj) throws ApiException {
         try {
             return AtivMobService.getInstance().proccessSugestoesPedido(cnpj);
+        } catch (Exception e) {
+            throw ExceptionUtils.handlerApiException(e);
+        }
+    }
+    
+    @POST
+    @Path("/cadastrar-evento}")
+    @Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+    public SugestaoPedido cadastrarEvento(EventDTO event) throws ApiException {
+        try {
+            return AtivMobService.getInstance().saveSugestaoPedido(event);
         } catch (Exception e) {
             throw ExceptionUtils.handlerApiException(e);
         }
