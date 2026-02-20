6a3efcc8f3feea03fba2834a1dfafc5a828fd529
Mateus Bastos <mateusbastos@vintglobal.com.br>
2026-02-19 22:49:29 -0300

    Alteracoes da paginacao

--- src/main/java/br/com/a2dm/spdmws/ws/ClientesWS.java
+++ src/main/java/br/com/a2dm/spdmws/ws/ClientesWS.java
@@ -1,7 +1,9 @@
 package br.com.a2dm.spdmws.ws;
 
 import java.math.BigInteger;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
@@ -11,6 +13,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import br.com.a2dm.brcmn.dto.ClienteDTO;
 import br.com.a2dm.brcmn.dto.ClienteIntegracaoDTO;
@@ -69,12 +72,20 @@ public class ClientesWS {
 	@POST
 	@Produces(MediaType.APPLICATION_JSON)
 	@Consumes(MediaType.APPLICATION_JSON)
-	public void processar(ClienteWebhookPayload clienteWebhookPayload) throws ApiException {
-		try {
-			OmieClienteService.getInstance().processar(clienteWebhookPayload);
-		} catch (Exception e) {
-			throw ExceptionUtils.handlerApiException(e);
-		}
+	public Response processar(ClienteWebhookPayload clienteWebhookPayload) throws ApiException {
+	    try {
+	    	if (clienteWebhookPayload.getMessageId() != null) {	    		
+	    		OmieClienteService.getInstance().processar(clienteWebhookPayload);
+	    	}
+
+	        Map<String, Object> responseJson = new HashMap<>();
+	        responseJson.put("resultCode", 0);
+	        responseJson.put("resultMsg", "");
+
+	        return Response.ok(responseJson).build();
+	    } catch (Exception e) {
+	        throw ExceptionUtils.handlerApiException(e);
+	    }
 	}
 }
 
