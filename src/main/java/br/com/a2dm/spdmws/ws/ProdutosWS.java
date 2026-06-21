8da410b9cb913c0fe9b71e3ed3835f3a05a91ca0
Lucas Gouveia <lucasgouveia@aptaretecnologia.com.br>
2026-06-21 11:30:43 -0300

    Webhook omni e carga

--- src/main/java/br/com/a2dm/spdmws/ws/ProdutosWS.java
+++ src/main/java/br/com/a2dm/spdmws/ws/ProdutosWS.java
@@ -1,21 +1,28 @@
 package br.com.a2dm.spdmws.ws;
 
 import java.math.BigInteger;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import br.com.a2dm.brcmn.dto.ProdutoDTO;
 import br.com.a2dm.brcmn.dto.ProdutoEstruturaDTO;
 import br.com.a2dm.spdm.entity.ClienteProduto;
 import br.com.a2dm.spdm.exception.ApiException;
 import br.com.a2dm.spdm.exception.ExceptionUtils;
+import br.com.a2dm.spdm.omie.payload.ProdutoWebhookPayload;
 import br.com.a2dm.spdm.omie.service.OmieProdutoEstruturaService;
 import br.com.a2dm.spdm.omie.service.OmieProdutoService;
+import br.com.a2dm.spdm.omie.service.OmieProdutoWebhookService;
 import br.com.a2dm.spdm.service.ClienteProdutoService;
 
 @Path("/produtos")
@@ -93,6 +100,25 @@ public class ProdutosWS {
 		try {
 			ProdutoEstruturaDTO response = OmieProdutoEstruturaService.getInstance().obterProdutoEstrutura(idProduto);
 			return response;
+		} catch (Exception e) {
+			throw ExceptionUtils.handlerApiException(e);
+		}
+	}
+
+	@POST
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response processar(ProdutoWebhookPayload produtoWebhookPayload) throws ApiException {
+		try {
+			if (produtoWebhookPayload.getMessageId() != null) {
+				OmieProdutoWebhookService.getInstance().processar(produtoWebhookPayload);
+			}
+
+			Map<String, Object> responseJson = new HashMap<>();
+			responseJson.put("resultCode", 0);
+			responseJson.put("resultMsg", "");
+
+			return Response.ok(responseJson).build();
 		} catch (Exception e) {
 			throw ExceptionUtils.handlerApiException(e);
 		}
